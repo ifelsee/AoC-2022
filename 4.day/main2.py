71e10a09bbc658b5a45fe5581cf13297e461acec
ifelsee <49848935+ifelsee@users.noreply.github.com>
2022-12-04 21:47:10 +0300

    day 4 - part 2

--- 4.day/main2.py
+++ 4.day/main2.py
@@ -1,21 +1,13 @@
 inp = [ i for i in [i.split() for i in open("inp","r")][:-2]]
-
 def main(inp) -> int:
     count = 0
     for i in inp:
         x,y = [j.split("-") for j in i[0].split(",")]
         a,b,c,d =  [int(f) for f in x+y]
         
-        #if a == c or a == d or b == c or b == d:
-        #   count+=1
 
-        x_range = [j for j in range(a,b+1)]
-        y_range = [j for j in range(c,d+1)]
-
-        for j in x_range:
-            if j in y_range:
-                count+=1
-                break
+        if a <= d and d >= c and b >= c :
+            count +=1
 
     return count 
         
