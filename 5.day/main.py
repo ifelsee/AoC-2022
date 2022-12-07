8d44cfaf235861764563adde0ef43df47968b0bc
ifelsee <49848935+ifelsee@users.noreply.github.com>
2022-12-07 05:06:23 +0300

    xInp

--- 5.day/main.py
+++ 5.day/main.py
@@ -1,32 +1,60 @@
-import re
 import sys 
 
 inp = [i.strip() for i in open("inp","r")]
-def matrix_inp(inp):
-    m_inp =[] 
 
-    for index_j, j in enumerate(inp[:8]): 
-        c=0; tmp = ""; arr = []
-        m_inp_tmp = ["   " for _ in range(8)]
 
-        for i in j:
-            c+=1; tmp+=i
 
-            if c >3 and i == " ":
-                arr.append(tmp[:-1])
-                c=0; tmp=""
+def xInp(hInp):
+    inp = hInp[:8]
+    inpMax = max([len(i) for i in inp])
+    transposeLst = [[] for _ in range(9)]
+
+    for index,row in enumerate(inp):
+
+        #normalization
+        if row.__len__() < inpMax:
+            row += " " * (inpMax-row.__len__())
+        
+        #parsing and transpose
+        prev,st = 0,4
+        for i in range(9):
+            if row[prev:st-1] != "   ":
+                transposeLst[i].append(row[prev:st-1])
+
+            prev = st
+            st += 4
             
-        for index_a, a in enumerate(arr):
-            m_inp_tmp[index_a] = a
-
-        m_inp.append(m_inp_tmp)
-    return m_inp
 
 
-if __name__ == "__main__": 
-    m_inp = matrix_inp(inp)
-    if len(sys.argv) == 1:
-        [print(_) for _ in m_inp]
+    
+
+    return [i[::-1] for i in transposeLst]   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__" and len(sys.argv) == 1:
+    mInp = xInp(inp)
+    print()
+    [print("#",end="") for _ in range(80)]
+    print()
+    [print(_) for _ in mInp]
 
 
 
